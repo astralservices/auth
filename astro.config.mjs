da9343ffdafd59a3893e9f3da880feae710f5f27
Jack Merrill <me@jackmerrill.com>
2022-07-21 16:14:34 -0500

    fix: add deno adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import turbolinks from "@astrojs/turbolinks";
 import partytown from "@astrojs/partytown";
 import node from "@astrojs/node";
 
+import deno from "@astrojs/deno";
+
 // https://astro.build/config
 export default defineConfig({
+  adapter: deno({
+    port: 4000,
+  }),
   integrations: [react(), tailwind(), turbolinks(), partytown(), node()],
 });
