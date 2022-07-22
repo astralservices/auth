4d5b02bff2c011bdf88df315568e02fa7c4951a2
Jack Merrill <me@jackmerrill.com>
2022-07-22 00:42:24 -0500

    fix: use right DSN

--- server.deno.js
+++ server.deno.js
@@ -2,7 +2,7 @@ import * as Sentry from "https://deno.land/x/sentry_deno/main.ts";
 import "./dist/server/entry.mjs";
 
 Sentry.init({
-  dsn: "https://dee54b8cee614b7eb6ae97dafff697b0@gt.astralapp.io/2",
+  dsn: "https://ae156eb36b1e4fcc8f3fcf48c7f4d933@gt.astralapp.io/3",
 });
 
 Sentry.captureMessage("Hello World");
